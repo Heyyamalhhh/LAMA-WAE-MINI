3206c654f0c6c3fc952ff0c7299848822d3225e3
ᝃ🌷／ī.am 々  𝐋𝐚𝐦𝐚 ⥮㍍⭒ <166611067+Heyyamalhhh@users.noreply.github.com>
2026-06-22 18:30:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    PREFIX: ".",
+    AUTO_READ_STATUS: false,
+    AUTO_REACT: false,
+    AUTO_TYPING: false,
+    REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+    ownerName: "ᝃ🌷／ī.am ༱  𝐋𝐚𝐦𝐚 ⥮㍍⭒",
+    ownerNumber: "918078346643",
+    botName: "ʟᴀᴍᴀ ᴡᴀᴇ xᴅ",
+    footer: "© ───   ʟᴀᴍᴀ   ───ᴩя𝝄𝜹𝝁ᴄтι𝝄𝜼",
+    watermark: "ʟ ᴀ ᴍ ᴀ ᴡ ᴀ ᴇ🌷", // ഡൗൺലോഡ് ഫയലുകൾക്ക് വേണ്ടിയുള്ള വാട്ടർമാർക്ക്
+    MENU_VIDEO: "https://xenocdn.xenocdn.workers.dev/a09e2dfd.mp4" 
+};
